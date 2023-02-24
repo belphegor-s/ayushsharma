9f676c99276ccf4fbe7473e28d957d545adb18a5
Ayush Sharma <ayush2162020@gmail.com>
2023-02-24 14:36:34 +0530

    updated designs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.scss";
+import { BsGithub, BsLinkedin, BsTwitter } from "react-icons/bs";
 
 export default function Home() {
   return (
@@ -8,9 +9,33 @@ export default function Home() {
         <title>Ayush Sharma</title>
         <meta name="description" content="Official App for the Domain" />
       </Head>
+
       <div className={styles.hero}>
         <h1>Ayush Sharma</h1>
-        <div>Full Stack Web Developer</div>
+        <div className={styles.subheading}>Full Stack Web Developer</div>
+        <div className={styles.profiles}>
+          <a
+            href="https://github.com/belphegor-s"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <BsGithub />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/ayush-sharma-2802/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <BsLinkedin />
+          </a>
+          <a
+            href="https://twitter.com/sharma_0502"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <BsTwitter />
+          </a>
+        </div>
       </div>
     </>
   );
