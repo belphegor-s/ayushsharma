44f017da37ab89889866c5f0d689925be12151d9
Ayush Sharma <ayush2162020@gmail.com>
2023-05-04 22:41:43 +0530

    feat: added projects and improvments

---
+++ components/Footer.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import styles from "../styles/Footer.module.scss";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles['tech-stack']}>
+        <div>
+            Built with&nbsp;&nbsp;<Image src="/img/nextjs.svg" alt="nextjs" width={18} height={18}/>
+        </div>
+        <div>•</div>
+        <div>Deployed on&nbsp;&nbsp;<Image src="/img/vercel.svg" alt="vercel" width={18} height={18}/></div>
+      </div>
+      <div className={styles.coffee}>Java Chip Frappe&nbsp;&nbsp;<Image src="/img/coffee.svg" alt="coffee" width={18} height={18}/>&nbsp;&nbsp;is love</div>
+    </footer>
+  )
+}
+export default Footer
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,23 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.scss";
-import { BsGithub, BsLinkedin, BsTwitter } from "react-icons/bs";
+import { BsGithub, BsLinkedin, BsTwitter, BsCaretDownFill } from "react-icons/bs";
+import { IoMail } from "react-icons/io5";
+import { useEffect, useRef, useState } from "react";
+import QUOTES from "../data/quotes.json";
+import PROJECTS from "../data/projects.json";
+import randomArrayItem from "@/util/getRandomArrItem";
+import ProjectCard from "@/components/ProjectCard";
+import Link from "next/link";
+import Footer from "@/components/Footer";
 
 export default function Home() {
+  const mainDivRef = useRef<HTMLDivElement | null>(null);
+  const [randomQuote, setRandomQuote] = useState<{[key:string] :any}>();
+  
+  useEffect(() => {
+    setRandomQuote(randomArrayItem(QUOTES.quotes));
+  }, []);
+
   return (
     <>
       <Head>
@@ -35,8 +50,33 @@ export default function Home() {
           >
             <BsTwitter />
           </a>
+          <a href="mailto:howdy@ayushsharma.me">
+            <IoMail />
+          </a>
+        </div>
+        <div className={styles['down-arrow']} onClick={() => mainDivRef.current?.scrollIntoView({behavior: 'smooth'})}>
+          <BsCaretDownFill/>
         </div>
       </div>
+
+      <div className={styles.main} ref={mainDivRef}>
+        <div className={styles.quote}>
+          <div>{randomQuote?.quote}</div>
+          <div><i>~ {randomQuote?.author}</i></div>
+        </div>
+        <div className={styles.projects}>
+          <h2>Projects</h2>
+          <div className={styles['project-cards-wrap']}>
+            {PROJECTS.map((project, i : number) =>
+              <Link key={`project-${i}`} href={project.link} target="_blank" rel="noreferrer">
+                <ProjectCard  data={project}/>
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
+
+      <Footer/>
     </>
   );
 }
