add8d971253f85f06d359db0591cf54b2d51b9c3
Ayush Sharma <ayush2162020@gmail.com>
2023-05-13 23:48:07 +0530

    fix: irregular ip issue

--- pages/api/public-ip.ts
+++ pages/api/public-ip.ts
@@ -1,13 +1,14 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import requestIp from "request-ip";
 
 const publicIP = (req: NextApiRequest, res: NextApiResponse) => {
     let success = false, msg = '';
-    const ipAddress = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    const ipAddress = requestIp.getClientIp(req);
 
     if(ipAddress) {
-        res.status(200).json({success: true, msg: 'Your Public IP', ip: ipAddress})
+        res.status(200).json({success: true, msg: 'Your Public IP', ip: ipAddress});
     } else {
-        res.status(404).json({success, msg: "Couldn't get the Public IP"})
+        res.status(404).json({success, msg: "Couldn't get the Public IP"});
     }
 }
 
