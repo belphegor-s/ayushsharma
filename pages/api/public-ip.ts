428e65bca036abbe2e093e15bc5a6eb4efbb153d
Ayush Sharma <ayush2162020@gmail.com>
2023-05-14 00:04:40 +0530

    feat: added geolocation api

--- pages/api/public-ip.ts
+++ pages/api/public-ip.ts
@@ -1,14 +1,19 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-const publicIP = (req: NextApiRequest, res: NextApiResponse) => {
+const publicIP = async (req: NextApiRequest, res: NextApiResponse) => {
     let success = false, msg = '';
-    const ipAddress = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-    console.log(req.headers['x-real-ip'])
 
-    if(ipAddress) {
-        res.status(200).json({success: true, msg: 'Your Public IP', ip: ipAddress, ip2: req.headers['x-real-ip'], ip3: req.socket.remoteAddress})
-    } else {
-        res.status(404).json({success, msg: "Couldn't get the Public IP"})
+    try {
+        const response = await fetch(`https://geolocation-db.com/json/${process.env.GEO_LOCATION_API_KEY}`)
+
+        if(response.ok && response.status === 200) {
+            const data = await response.json();
+            res.status(200).json({success: true, msg: 'IP Details fetched successfully', ...data})
+        } else {
+            res.status(404).json({success, msg: "Couldn't get the Public IP"})
+        }
+    } catch(e) {
+        console.log(`Error fetching IP Details -> ${e}`);
     }
 }
 
