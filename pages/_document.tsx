6bac932cd35d675045642bc1c3d3c667a2fe4868
Ayush Sharma <ayush2162020@gmail.com>
2023-12-14 13:07:21 +0530

    style: updated favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,17 +4,14 @@ export default function Document() {
 	return (
 		<Html lang="en">
 			<Head>
+				<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
 				<link rel="manifest" href="/manifest.json" />
 				<link rel="apple-touch-icon" href="/logo192.png" />
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" />
-				<link
-				href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet"/>
-				<link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital@0;1&display=swap" rel="stylesheet"/>
-				<link
-				rel="stylesheet"
-				href="https://cdn.jsdelivr.net/npm/bootstrap-reboot@4.5.6/reboot.min.css"
-				/>
+				<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+				<link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital@0;1&display=swap" rel="stylesheet" />
+				<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-reboot@4.5.6/reboot.min.css" />
 			</Head>
 			<body>
 				<Main />
