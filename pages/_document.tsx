41ac5e420f4a86d4b2b92b734ca95de48a84c532
Ayush Sharma <ayush2162002@gmail.com>
2024-05-09 11:35:24 +0530

    refactor: remove iframe

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,13 +12,6 @@ export default function Document() {
 				<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet" />
 
 				<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-reboot@4.5.6/reboot.min.css" />
-
-				<link rel="stylesheet" href="https://chatzy-kb-store.s3.amazonaws.com/icons/5ab07987-b5db-477c-82ff-1287e0883acb" />
-				<script
-					src="https://chatzy-kb-store.s3.amazonaws.com/icons/cd8b748a-158a-4fa0-831c-742a733ed3c7"
-					id="b8353106-d712-4d99-929f-cbc9fe2a55d2"
-					className="chatzy_widget_script"
-					defer></script>
 			</Head>
 			<body>
 				<Main />
